65de28f4fc8c6cd1246dff75b5f14519a4fb8e10
Riley de Man <rileydeman@gmail.com>
2025-05-13 15:08:27 +0200

    Start at the header
    Start at the header

--- js/app.js
+++ js/app.js
@@ -18,3 +18,24 @@ fetch('core/nav.html')
         document.getElementById('nav').innerHTML = data;
     })
     .catch(error => console.error('Error loading content:', error));
+
+
+
+let lightTheme = true;
+
+function changeTheme() {
+   let body = document.querySelector('body');
+   let headerLogo = document.getElementById("h-logo");
+
+   if (lightTheme) {
+       body.setAttribute("data-theme", "dark");
+       headerLogo.setAttribute("src", "/assets/img/logo-black.png");
+
+       lightTheme = !lightTheme;
+   } else {
+       body.setAttribute("data-theme", "light");
+       headerLogo.setAttribute("src", "/assets/img/logo-white.png");
+
+       lightTheme = !lightTheme;
+   }
+}
