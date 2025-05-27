acbfd2743021dfb85c040d60dce362300816f7ba
Riley de Man <rileydeman@gmail.com>
2025-05-27 14:13:04 +0200

    Line up grid + api connection
    Line up grid + api connection

--- js/line-up.js
+++ js/line-up.js
@@ -11,6 +11,59 @@ for (let i = 0; i < daySectorItems.length; i++) {
 
         daySectorItems[i].classList.add("dsi-selected");
         currentDay = daySectorItems[i].getAttribute("data-day");
-        //getTimetable();
+        getLineUp();
     })
+}
+
+// Line Up
+let lineUpData;
+
+function getLineUp() {
+    let requestUrl = `${API_BASEURL}v1/line-up/`;
+
+    if (currentDay != "ALL") {
+        requestUrl += `?day=${currentDay}`;
+    }
+
+    emptyLineUp();
+
+    fetch(requestUrl, {
+        method: "GET"
+    })
+        .then(response => {
+            // Check if the request was successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            // Parse the response as JSON
+            return response.json();
+        })
+        .then(data => {
+            // Handle the JSON data
+            // console.log(data);
+            lineUpData = data.payload;
+            updateLineUp();
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+getLineUp();
+
+function updateLineUp() {
+    let lineupGrid = document.getElementById("lineupGrid");
+
+    lineUpData.forEach((act, i) => {
+        lineupGrid.innerHTML += '<div class="actCard"><img class="actImg" src="' + act.img.square + '" alt=""><div class="actNameDiv"><p class="actName">' + act.name + '</p></div></div>';
+    })
+}
+
+function emptyLineUp() {
+    let actCards = document.getElementsByClassName("actCard");
+
+    while (actCards.length > 0) {
+        actCards[0].remove();
+    }
 }
