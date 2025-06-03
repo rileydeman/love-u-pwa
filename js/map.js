6bcca32481a40725abc52278cdc9491f53b4db31
Riley de Man <rileydeman@gmail.com>
2025-06-03 13:35:37 +0200

    Added zoom and return to north button to the map.
    Added zoom and return to north button to the map.

--- js/map.js
+++ js/map.js
@@ -1,15 +1,17 @@
 mapboxgl.accessToken = secrets.api.mapbox;
 
 const mapBounds = [
-    [5.053652, 52.070356],
-    [5.068031, 52.079411]
+    [5.053652, 52.070356], // South-West
+    [5.064011, 52.079448] // North-East
 ];
 
 const map = new mapboxgl.Map({
     container: 'map', // container id
     //style: 'mapbox://styles/mapbox/streets-v12',
     style: 'mapbox://styles/rileydeman/cmb7z5apf00qd01sc7q9431p5', // style URL
-    center: [5.059045, 52.075377], // starting position [lng, lat]
+    center: [5.059045, 52.075577], // starting position [lng, lat]
     zoom: 15.25, // starting zoom
     maxBounds: mapBounds
 });
+
+map.addControl(new mapboxgl.NavigationControl({ showCompass: true }), 'top-right');
